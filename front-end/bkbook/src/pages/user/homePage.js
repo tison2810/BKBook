50f116a696ba25f1b4552a52dcfca3ef5f29c06c
tison2810 <son.nguyenthai@hcmut.edu.vn>
2023-11-11 20:30:09 +0700

    hehe

---
+++ front-end/bkbook/src/pages/user/homePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to BKBook</h1>
+      <p>Explore our collection of books and connect with other book lovers!</p>
+    </div>
+  );
+}
+
+export default HomePage;
