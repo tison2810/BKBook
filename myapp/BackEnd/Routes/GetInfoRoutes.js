23c9e0f5c04e6a793dd8f54e5ee61119ec164524
an114116 <131609524+an114116@users.noreply.github.com>
2024-01-01 09:25:23 +0700

    Update

--- myapp/BackEnd/Routes/GetInfoRoutes.js
+++ myapp/BackEnd/Routes/GetInfoRoutes.js
@@ -5,5 +5,7 @@ const GetInfoController = require('../Controller/GetInfoController');
 router.get('/GetAdmin', GetInfoController.GetAdmin);
 router.get('/GetOrder', GetInfoController.GetOrder);
 router.get('/GetOrderConfirm', GetInfoController.GetOrderConfirm);
+router.get('/GetListBook', GetInfoController.GetListBook);
+router.get('/GetBook', GetInfoController.GetBook);
 
 module.exports = router;
