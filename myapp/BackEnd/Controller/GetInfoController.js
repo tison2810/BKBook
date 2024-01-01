ce79b0344e35e27fa443d5e457a0389ad379282e
an114116 <131609524+an114116@users.noreply.github.com>
2024-01-01 09:23:11 +0700

    Update GetInfoController.js
    up

--- myapp/BackEnd/Controller/GetInfoController.js
+++ myapp/BackEnd/Controller/GetInfoController.js
@@ -4,33 +4,55 @@ exports.GetAdmin = async (req, res, next) => {
       const { employee_ID } = req.params;
       const value1= [employee_ID];
       const results = await query(query1,value1);
-      res.status(200).json(results)
+      res.status(200).json(results);
     } catch (error) {
       next(error);
     }
   }
 
-  exports.GetOrder = async (req, res, next) => {
-    try {
-      const query1 =`SELECT * FROM donhang WHERE ID = ?`;
-      const { order_ID } = req.params;
-      const value1 = [order_ID];
-      const results = await query(query1,value1);
-      res.status(200).json(results)
-      } catch (error) {
-        next(error);
-      }
-  }
+exports.GetOrder = async (req, res, next) => {
+try {
+  const query1 =`SELECT * FROM donhang WHERE SoDienThoai = ?`;
+  const { SĐT } = req.params;
+  const value1 = [SĐT];
+  const results = await query(query1,value1);
+  res.status(200).json(results);
+} catch (error) {
+  next(error);
+}
+}
 
-  exports.GetOrderConfirm = async (req, res, next) => {
-    try {
-      const query1 =`SELECT * 
-                    FROM donhang
-                    WHERE XacNhan IS NULL
-                    `;
-      const results = await query(query1);
-      res.status(200).json(results)
-      } catch (error) {
-        next(error);
-      }
+exports.GetOrderConfirm = async (req, res, next) => {
+try {
+  const query1 =`SELECT * 
+                FROM donhang
+                WHERE XacNhan = 'N'
+                `;
+  const results = await query(query1);
+  res.status(200).json(results);
+} catch (error) {
+  next(error);
+}
+}
+
+exports.GetListBook = async (req, res, next) => {
+try {
+  const query1 =`SELECT * FROM sach`;
+  const results = await query(query1);
+  res.status(200).json(results)
+  } catch (error) {
+    next(error);
   }
+}
+
+exports.GetBook = async (req, res, next) => {
+try {
+  const query1 =`SELECT * FROM sach WHERE ID = ?`;
+  const { book_ID } = req.params;
+  const value1 = [book_ID];
+  const results = await query(query1,value1);
+  res.status(200).json(results);
+  } catch (error) {
+    next(error);
+  }
+}
